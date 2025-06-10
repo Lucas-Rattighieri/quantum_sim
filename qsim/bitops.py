97a89000c60afc103aca6f4c6f5e6d67d8d39dbc
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2025-06-10 20:40:26 -0300

    Update bitops.py

--- qsim/bitops.py
+++ qsim/bitops.py
@@ -1,34 +1,120 @@
-import torch
 
-def ligar_bit(n, p):
-    return n | (1 << p)
+def ligar_bit(num, p : int):
+    """
+    Define o bit na posição p como 1 no número num.
+
+    Parâmetros:
+    - num (int): número inteiro de entrada.
+    - p (int): posição do bit a ser ativada.
+
+    Retorna:
+    - int: novo número com o bit p ligado (1).
+    """
+    return num | (1 << p)
 
 
-def desligar_bit(n, p):
-    return n & (~ (1 << p))
+def desligar_bit(num, p : int):
+    """
+    Define o bit na posição p como 0 no número num.
+
+    Parâmetros:
+    - num (int): número inteiro de entrada.
+    - p (int): posição do bit a ser desativada.
+
+    Retorna:
+    - int: novo número com o bit p desligado (0).
+    """
+    return num & (~ (1 << p))
 
 
-def bit(indice, i):
-    return (indice >> i) & 1
+def bit(num, i : int):
+    """
+    Retorna o valor do bit na posição i do número num.
+
+    Parâmetros:
+    - num (int): número inteiro de entrada.
+    - i (int): posição do bit a ser lida.
+
+    Retorna:
+    - int: valor do bit (0 ou 1).
+    """
+    return (num >> i) & 1
 
 
-def contar_bits(indice, L):
+def contar_bits(num, L : int):
+    """
+    Conta o número de bits iguais a 1 nos L bits menos significativos de num.
+
+    Parâmetros:
+    - num (int): número inteiro de entrada.
+    - L (int): número de bits considerados.
+
+    Retorna:
+    - int: quantidade de bits 1 entre os L bits menos significativos.
+    """
     uns = 0
     for i in range(L):
-        uns += (indice >> i) & 1
+        uns += (num >> i) & 1
     return uns
 
 
-def translacao(num, d : int, L : int):
+def permutar_bits(num, i : int, j : int):
+    """
+    Troca os bits nas posições i e j do número num.
+
+    Parâmetros:
+    - num (int): número inteiro de entrada.
+    - i (int): primeira posição de bit.
+    - j (int): segunda posição de bit.
+
+    Retorna:
+    - int: número com os bits i e j permutados (se forem diferentes).
+    """
+    mask = (((num >> i) ^ (num >> j)) & 1)
+    return num ^ ((mask << i) | (mask << j))
+
+
+def translacao(num, d: int, L: int):
+    """
+    Aplica uma translação cíclica de d posições para a direita nos L bits de num.
+
+    Parâmetros:
+    - num (int): número inteiro de entrada.
+    - d (int): número de posições a deslocar.
+    - L (int): número total de bits considerados.
+
+    Retorna:
+    - int: número com os bits deslocados ciclicamente.
+    """
     d %= L
-    return (num >> (L-d)) | ((num << d) & ((1 << L) - 1))
+    return (num >> (L - d)) | ((num << d) & ((1 << L) - 1))
 
 
-def inversao(num, L : int):
-    return (~ num) & ((1 << L) -1)
+def inversao(num, L: int):
+    """
+    Inverte todos os bits nos L bits menos significativos de num.
+
+    Parâmetros:
+    - num (int): número inteiro de entrada.
+    - L (int): número de bits considerados.
+
+    Retorna:
+    - int: número com os L bits invertidos.
+    """
+    return (~ num) & ((1 << L) - 1)
 
 
 def reflexao(num, L: int):
+    """
+    Reflete a ordem dos L bits de num (espelhamento da bitstring).
+
+    Parâmetros:
+    - num (int): número inteiro de entrada.
+    - L (int): número total de bits.
+
+    Retorna:
+    - int: número com a ordem dos bits invertida (refletida).
+    """
     n1 = 0
     for i in range(L):
         n1 |= ((num >> i) & 1) << (L - 1 - i)
