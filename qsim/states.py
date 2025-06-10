e753e9d4e6e05a21400e55760235a04db2de0251
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2025-06-10 20:25:39 -0300

    Update states.py

--- qsim/states.py
+++ qsim/states.py
@@ -1,11 +1,23 @@
 import torch
 
 from .config import device, dtype
+from .bitops import contar_bits
 
-
-#@title Estados iniciais
 
 def superposicao_uniforme(L, num_estados = 1):
+    """
+    Gera um vetor de estado representando uma superposição uniforme sobre todos os estados base de L qubits.
+
+    Se num_estados > 1, gera múltiplos vetores de superposição uniforme em colunas.
+
+    Parâmetros:
+    - L (int): número de qubits.
+    - num_estados (int, opcional): número de vetores de estado a serem gerados. Default é 1.
+
+    Retorna:
+    - psi (torch.Tensor): vetor(s) de estado(s) com amplitudes iguais para todos os estados base,
+    normalizados para norma 1.
+    """
 
     if num_estados == 1:
         psi = torch.ones(2**L, dtype=dtype, device=device)
@@ -17,35 +29,69 @@ def superposicao_uniforme(L, num_estados = 1):
     return psi
 
 
-def base_z(L):
-    return torch.eye(2**L, dtype=dtype, device=device)
-
-
-def base_x(L):
-    base = torch.arange(2**L, dtype=torch.int64, device=device).unsqueeze(1)  # shape: (2**L, 1)
-
-    matriz = base.repeat(1, 2**L)
-
-    produto = 1 - 2 * (contar_bits(matriz.T & matriz, L) & 1)
-
-    psi = produto / torch.sqrt(torch.tensor(2 ** L, device=device))
-
-    return psi
-
-
-
 def autoestado_z(L, i):
+    """
+    Gera o autoestado do operador soma dos Z, ∑_a Z_a, correspondente ao estado base computacional |i⟩.
+
+    Parâmetros:
+    - L (int): número total de qubits.
+    - i (int): inteiro representando o estado base |i⟩.
+
+    Retorna:
+    - psi (torch.Tensor): vetor de estado com 1 na posição i e zero nas demais, representando |i⟩.
+    """
+
     psi = torch.zeros(2**L, dtype=dtype, device=device)
     psi[i] = 1
     return psi
 
 
 def autoestado_x(L, i):
+    """
+    Gera um autoestado do operador soma dos X, ∑_a X_a, para um sistema de L qubits.
+
+    O parâmetro `i` representa uma bitstring na base {|+⟩ = (|0⟩ + |1⟩)/2, |-⟩ = (|0⟩ - |1⟩)/2}, onde (+) é codificado como 0 
+    e (-) como 1 (Ex: i = 3, L = 5 -> |+++--⟩). 
+
+    Parâmetros:
+    - L (int): número total de qubits.
+    - i (int): inteiro representando a bitstring na base {|+⟩, |-⟩}.
+
+    Retorna:
+    - psi (torch.Tensor): vetor de estado normalizado correspondente ao autoestado do operador ∑_a X_a.
+    """
+
     indice = torch.arange(2**L, dtype=torch.int64, device=device)
 
     novo_indice = indice & i
     produto = 1 - 2 * (contar_bits(novo_indice, L) & 1)
 
     psi = produto / torch.sqrt(torch.tensor(2 ** L, device=device))
+
+    return psi
+
+
+def autoestado_y(L, i):
+    """
+    Gera um autoestado do operador soma dos Y, ∑_a Y_a, para um sistema de L qubits.
+
+    O parâmetro `i` representa uma bitstring na base {|+i⟩ = (|0⟩ + i|1⟩)/2, |-i⟩ = (|0⟩ - i|1⟩)/2}, onde (+i) é codificado como 0 
+    e (-i) como 1 (Ex: i = 3, L = 5 -> |(+i)(+i)(+i)(-i)(-i)⟩). 
+
+    Parâmetros:
+    - L (int): número total de qubits.
+    - i (int): inteiro representando a bitstring na base {|+i⟩, |-i⟩}.
+
+    Retorna:
+    - psi (torch.Tensor): vetor de estado normalizado correspondente ao autoestado do operador ∑_a Y_a.
+    """
+
+    indice = torch.arange(2**L, dtype=torch.int64)
+    bits_contados = contar_bits(indice, L)
+
+    novo_indice = indice & i
+    produto = produto = (1 - 2 * ((contar_bits(novo_indice, L) ^ (bits_contados >> 1)) & 1)) * (1 + (bits_contados & 1) * (1j - 1))
+
+    psi = produto / torch.sqrt(torch.tensor(2 ** L))
 
     return psi
