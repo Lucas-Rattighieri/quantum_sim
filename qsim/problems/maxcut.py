e5c08fea965462c4e2c698af26572a68934635a1
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2025-06-21 00:03:45 -0300

    Update maxcut.py

--- qsim/problems/maxcut.py
+++ qsim/problems/maxcut.py
@@ -3,6 +3,30 @@ from .. import diagonal as qd
 from .. import bitops as qb
 from .. import config as qconfig
 
+
+def indice_maxcut(num_vertices : int, vertice_i : int, fixar_subconjunto : bool = False):
+    """
+    Retorna o índice da posição do bit associado ao vértice `vertice_i` na representação
+    binária do estado para o problema MaxCut.
+
+    Parâmetros:
+        num_vertices (int): número total de vértices (qubits)
+        vertice_i (int): índice do vértice (0 ≤ vertice_i < num_vertices)
+        fixar_subconjunto (bool): se True, assume que o vértice 0 está fixado e não é codificado
+
+    Retorna:
+        int: índice do bit correspondente à variável binária associada ao vértice `vertice_i`.
+             Retorna -1 se `vertice_i` estiver fixado (ou seja, vertice_i == 0 e fixar_subconjunto=True).
+    """
+
+    if fixar_subconjunto:
+        if vertice_i == 0:
+            return -1
+        return num_vertices - vertice_i
+    else:
+        return num_vertices - vertice_i - 1
+
+
 def Hmaxcut(L: int, w, fixar_subconjunto : bool =False):
     """
     Constrói a diagonal do Hamiltoniano do MaxCut com codificação Z ∈ {±1}.
@@ -14,7 +38,7 @@ def Hmaxcut(L: int, w, fixar_subconjunto : bool =False):
             representado por 0
 
     Retorna:
-        Tensor: vetor de dimensão 2^L com a diagonal do Hamiltoniano
+        Tensor: vetor de dimensão 2^L (ou 2^(L - 1) se fixado) com a diagonal do Hamiltoniano
     """
 
     Ll = L - 1 if fixar_subconjunto else L
@@ -24,18 +48,20 @@ def Hmaxcut(L: int, w, fixar_subconjunto : bool =False):
     for i in range(L):
         for j in range(i + 1, L):  
             if w[i, j] != 0:
+                ki = indice_maxcut(L, i, fixar_subconjunto)
+                kj = indice_maxcut(L, j, fixar_subconjunto)
                 if fixar_subconjunto:
                     if i == 0:
-                        H += qd.cadeia_z(Ll, [j-1], w[i, j])
+                        H += qd.cadeia_z(Ll, [kj], w[i, j])
                     else:
-                        H += qd.cadeia_z(Ll, [i-1, j-1], w[i, j])
+                        H += qd.cadeia_z(Ll, [ki, kj], w[i, j])
                 else:
-                    H += qd.cadeia_z(Ll, [i, j], w[i, j])
+                    H += qd.cadeia_z(Ll, [ki, kj], w[i, j])
 
     return H
 
 
-def particao_maxcut(num: int, L: int, fixar_subconjunto : bool =False):
+def particao_maxcut(num: int, L: int, fixar_subconjunto : bool = False):
     """
     Dado um inteiro que representa a configuração binária do MaxCut (como um estado base),
     retorna os subconjuntos A e B definidos por essa configuração.
